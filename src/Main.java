9757e7625063430611a1cd1e2e493b1a5a458feb
Kate Bakhtina <almazniegori@gmail.com>
2024-10-12 00:02:41 +0300

    add_change_to_result

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,6 @@ public class Main {
         BonusMilesService service = new BonusMilesService();
         int price = 10_000;
         int miles = service.calculate(price);
-        System.out.println("Вам начислено: " + miles);
+        System.out.println("Вам начислено: " + miles + " бонусов");
     }
 }
